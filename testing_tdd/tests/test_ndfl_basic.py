6404c54fb7ac25258e03e48de4dd921fe14a868f
anton krotov <twinkidr@gmail.com>
2026-03-11 17:07:59 +0800

    add files

--- testing_tdd/tests/test_ndfl_basic.py
+++ testing_tdd/tests/test_ndfl_basic.py
@@ -1,4 +1,8 @@
 from ndfl import calculate_ndfl_tax
 
-def test_ndfl():
-    assert test_ndfl() == None
+def test_ndfl_tier_1():
+    assert calculate_ndfl_tax(500_000) == 65_000
+def test_ndfl_tier_2():
+    assert calculate_ndfl_tax(4_000_000) == 512_000
+def test_ndfl_tier_3():
+    assert calculate_ndfl_tax(10_000_000) == 1_602_000
