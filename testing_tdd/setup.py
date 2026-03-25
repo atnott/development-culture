66fdea41a3fc69c1178e573b6be0460205c373ed
anton krotov <twinkidr@gmail.com>
2026-03-25 19:44:26 +0800

    add makefile

--- testing_tdd/setup.py
+++ testing_tdd/setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ndfl',
-    version='0.0.1',
+    name='atnott-ndfl-test-123',
+    version='0.0.2',
+    long_description="Tax calculator",
+    long_description_content_type="text/markdown",
     package_dir={'': 'src'},
     packages=find_packages(where='src')
 )
--- testing_tdd/src/ndfl.py
+++ testing_tdd/src/ndfl.py
@@ -1,24 +1,10 @@
-def calculate_ndfl_tax(income):
-    tiers = [
-        (0.0, 0.0, 0.13),
-        (2_400_000.0, 312_000.0, 0.15),
-        (5_000_000.0, 702_000.0, 0.18)
-    ]
-    for start, addition, rate in tiers:
-        if income > start:
-            return addition + (income - start) * rate
-        return
-
-    if income < 2_400_000:
-        return income * 0.13
-    elif income < 5_000_000:
-        return (
-                2_400_000 * 0.13 +
-                (income - 2_400_000) * 0.15
-        )
-    elif income < 20_000_000:
-        return (
-                2_400_000 * 0.13 +
-                2_600_000 * 0.15 +
-                (income - 5_000_000) * 0.18
-        )
+def calculate_ndfl_lax(param):
+    tier=[
+        (0.0,0.0,0.13),
+        (2_400_000.0,312_000.0, 0.15),
+        (5_000_000.0,762_000.0,0.18),
+          ]
+    for start,addition, rate in tier[::-1]:
+        if param>start:
+            return addition+(param-start)*rate
+    return
