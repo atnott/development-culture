8734d4fe5b771df1fbca44bf6a293f632ac1f8b3
anton krotov <twinkidr@gmail.com>
2026-04-01 16:11:05 +0800

    add new branch

--- testing_tdd/setup.py
+++ testing_tdd/setup.py
@@ -1,10 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import (
+    setup, 
+    find_packages
+)
 
 setup(
-    name='atnott-ndfl-test-123',
-    version='0.0.2',
-    long_description="Tax calculator",
-    long_description_content_type="text/markdown",
-    package_dir={'': 'src'},
-    packages=find_packages(where='src')
+    name = "ndfl",
+    version = "0.0.1",
+    long_description = "Tax calculator",
+    long_description_content_type = "text/markdown",
+    package_dir = {"": "src"},
+    packages = find_packages(where="src")
 )
+
